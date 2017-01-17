07968f52683946e2e8a73b291627aad2ca262888
AMD-Grifon <djkoloda@yandex.ru>
2017-01-18 01:52:24 +0300

    Fix TWRP_THEME

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -189,7 +189,7 @@ BOARD_SCREENRECORD_LANDSCAPE_ONLY := true
 -include device/samsung/wave/Android.mk
 
 # TWRP Flags
-TW_THEME := portrait_hdpi
+TW_THEME := portrait_mdpi
 TW_NO_BATT_PERCENT := true
 TW_NO_REBOOT_BOOTLOADER := true
 RECOVERY_SDCARD_ON_DATA := true
